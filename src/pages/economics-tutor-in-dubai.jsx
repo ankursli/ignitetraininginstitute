8b8c2c5d43fa62a1151538a8201075a2d5e885a4
ankuesli <ankur.ankp@gmail.com>
2026-02-10 09:51:25 +0530

    refactor: split InfoCard component into InfoCardLeft and InfoCardForm for improved modularity.

--- src/pages/economics-tutor-in-dubai.jsx
+++ src/pages/economics-tutor-in-dubai.jsx
@@ -198,6 +198,7 @@ const EconomicsTutorsInDubai = ({ headerHeight }) => {
                                 width="1200"
                                 height="800"
                                 className="hero-img"
+                                style={{ opacity: 1, visibility: 'visible' }}
                             />
                         </picture>
 
